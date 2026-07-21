945b4fa6d9a8b4a39efb1fac658bdcfb48cedd94
Nadr59 <117353272+Nadr59@users.noreply.github.com>
2026-07-21 10:22:51 +0300

    Add files via upload

--- app/src/main/java/com/apppair/AppPairApplication.kt
+++ app/src/main/java/com/apppair/AppPairApplication.kt
@@ -1,1 +1,11 @@
+package com.apppair
 
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppPairApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
